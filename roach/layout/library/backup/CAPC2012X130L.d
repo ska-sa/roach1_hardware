b677881dc26ecdba043875dadd62d7b46f0a528a
etienne <etienne@51f75b46-3331-0410-b433-c6c2fccb8461>
2008-05-29 15:19:53 +0000

    git-svn-id: https://katfs.kat.ac.za/svnROACH/elec@832 51f75b46-3331-0410-b433-c6c2fccb8461

---
+++ roach/layout/library/backup/CAPC2012X130L.d
@@ -0,0 +1,26 @@
+*PADS-LIBRARY-PCB-DECALS-V2007*
+
+CAPC2012X130L     I 1000  1000  0 2 2 0 2 1 0
+TIMESTAMP 1999.08.31.15.21.39
+-70   150   0     0 100   10    1 0 34 "Default Font"
+REF-DES
+-70   150   0     0 100   10    1 32 35 "Default Font"
+PART-TYPE
+OPEN   4 10 0 -1
+-15   45   
+-95   45   
+-95   -45  
+-15   -45  
+OPEN   4 10 0 -1
+15    45   
+95    45   
+95    -45  
+15    -45  
+T-45   0     -45   0     1
+T45    0     45    0     2
+PAD 0 3 P 0    
+-2 50  RF  0    60 0  
+-1 0   R  
+0  0   R  
+
+*END*
